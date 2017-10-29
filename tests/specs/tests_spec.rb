45da17ae69750909e764b4588d7a17a9ee9334f6
Joshua Cherry <joshuarcherry@gmail.com>
2017-10-29 18:39:39 -0400

    Initial Commit

---
+++ tests/specs/tests_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+control '01' do
+  impact 1.0
+  title 'Verify grafana '
+  desc 'Ensures grafana service and web is up and running'
+
+  describe service('grafana-server') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
+  describe user("grafana") do
+    it { should exist }
+  end
+  describe group("grafana") do
+    it { should exist }
+  end
+
+  describe port(3000) do
+    it { should be_listening }
+    its('processes') {should include 'grafana-server'}
+  end
+  describe http('http://127.0.0.1:3000/login') do
+    its('status') { should cmp 200 }
+  end
+
+end
